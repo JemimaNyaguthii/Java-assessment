7188f6cc855b657be9fdd584bd2dc3af1cee09f8
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-04-11 08:59:42 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,65 @@
+// Write a function that takes in a string and returns it when reversed
+// let food = “eating”
+function returns(food){
+    let food="eating"
+    console.log(food.reverse())
+}
+// Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 
+function arr1(num){
+    if (num.length<=1){
+        return num
+    }
+    let middle=Math.floor(num.lenght/2)
+    let left=num.slice(0,middle)
+    let right =num.slice(middle)
+    return sortedArray(divide (left),divide (right)) 
+}
+function sortedArray(left,right)
+let empty=[]
+while(            
+    num.left && num.right 
+    )
+
+    if (
+        num.left<=middle =empty.shift()
+    )
+    
+
+
+
+
+// Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”let
+years %=4
+if(num.)
+
+// Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+// let numArray = [12,87,45,75,23,64,73]
+function arrnum( numArray){
+    forEach(
+        numArray *=4
+    )
+    return{
+        numArray
+    }
+
+    let numArray=[12,87,45,75,23,64,73]
+    console.log(numArray)
+}
+
+
+    
+
+
+
+
+   
+
+
+
+
+
